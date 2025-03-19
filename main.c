4496387679ea6b1d8db7518f0f6459837eadb91b
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-03-19 16:49:47 -0300

    Update main.c

--- main.c
+++ main.c
@@ -24,3 +24,20 @@ void setGoals(struct SoccerPlayer* player, int goals) {
 void setAssists(struct SoccerPlayer* player, int assists) {
     player->assists = assists;
 }
+
+void printData(struct SoccerPlayer* player) {
+    printf("NAME: %s | MATCHES: %d | GOALS: %d | ASSISTS: %d", player->name, player->matches, player->goals, player->assists);
+}
+
+int main() {
+    struct SoccerPlayer cr7;
+
+    setName(&cr7, "crisrondo7");
+    setMatches(&cr7, 1580);
+    setGoals(&cr7, 980);
+    setAssists(&cr7, 728);
+
+    printData(&cr7);
+    
+    return 0;
+}
