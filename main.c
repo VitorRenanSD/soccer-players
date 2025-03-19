2b86640de36272086b91cbfe2cf807f5125144b3
Vitor RenanSD <vitorrenansouzadias@gmail.com>
2025-03-19 17:11:06 -0300

    Update main.c

--- main.c
+++ main.c
@@ -8,6 +8,7 @@ struct SoccerPlayer {
     int assists;
 };
 
+// Setter methods
 void setName(struct SoccerPlayer* player, const char* name) {
     strncpy(player->name, name, sizeof(player->name) - 1);
     player->name[sizeof(player->name) - 1] = '\0';
@@ -25,19 +26,50 @@ void setAssists(struct SoccerPlayer* player, int assists) {
     player->assists = assists;
 }
 
+
+// Print all player data
 void printData(struct SoccerPlayer* player) {
-    printf("NAME: %s | MATCHES: %d | GOALS: %d | ASSISTS: %d", player->name, player->matches, player->goals, player->assists);
+    printf("NAME: %s | MATCHES: %d | GOALS: %d | ASSISTS: %d", 
+    player->name, player->matches, player->goals, player->assists);
 }
+
+
+// Sum 2 players statistics
+void sumPlayerData(struct SoccerPlayer* player1, struct SoccerPlayer* player2) {
+    int totalMatches = player1->matches + player2->matches;
+    int totalGoals = player1->goals + player2->goals;
+    int totalAssists = player1->assists + player2->assists;
+
+    printf("%s + %s | MATCHES: %d | GOALS: %d | ASSISTS: %d",
+    player1->name, player2->name, totalMatches, totalGoals, totalAssists);
+}
+
+
+// Tests if a soccer player is good
+int goodPlayer(struct SoccerPlayer* player) {
+    if (strcmp(player->name, "CR7") == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 
 int main() {
     struct SoccerPlayer cr7;
+    struct SoccerPlayer messi;
 
-    setName(&cr7, "crisrondo7");
-    setMatches(&cr7, 1580);
-    setGoals(&cr7, 980);
+    setName(&cr7, "CR7");
+    setMatches(&cr7, 1270);
+    setGoals(&cr7, 926);
     setAssists(&cr7, 728);
 
-    printData(&cr7);
-    
+    setName(&messi, "Messi");
+    setMatches(&messi, 1086);
+    setGoals(&messi, 862);
+    setAssists(&messi, 580);
+
+    sumPlayerData(&cr7, &messi);
+
     return 0;
 }
